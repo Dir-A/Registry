9d6f6e42cee51090f285f9fdc5c6fb8ee430cff9
Dir-A <g2576612205@gmail.com>
2024-05-19 14:52:51 +0800

    Update xmake.lua

--- packages/z/zqf-zut/xmake.lua
+++ packages/z/zqf-zut/xmake.lua
@@ -1,22 +1,22 @@
-package(zqf-zut)
-    set_homepage(https://github.com/Dir-A/Zut)
-    set_description(a personal cpp utility library)
-    add_urls(https://github.com/Dir-A/Zut.git)
+package("zqf-zut")
+    set_homepage("https://github.com/Dir-A/Zut")
+    set_description("a personal cpp utility library")
+    add_urls("https://github.com/Dir-A/Zut.git")
 
-    add_versions(v1.0, 37741d6306208c2fc894911fcde4a859a28846fa)
+    add_versions("v1.0", "37741d6306208c2fc894911fcde4a859a28846fa")
 
     -- on load, get and set configs
-    on_load(windows, mingw, function (package)
+    on_load("windows", "mingw", function (package)
         --
     end)
 
     -- on install, check configs and compile
-    on_install(windows, mingw, function (package)
+    on_install("windows", "mingw", function (package)
         local configs = {}
-        if packageconfig(shared) then
-            configs.kind = shared
+        if packageconfig("shared") then
+            configs.kind = "shared"
         end
-        import(package.tools.xmake).install(package, configs)
+        import("package.tools.xmake").install(package, configs)
     end)
 
     -- on test
