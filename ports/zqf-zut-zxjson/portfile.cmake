0a2858823b43f7945763faa5f1f39e7b859b5de5
Dir-A <g2576612205@gmail.com>
2025-09-21 21:21:21 +0800

    add: zqf-zut-zxjson

--- ports/zqf-zut-zxjson/portfile.cmake
+++ ports/zqf-zut-zxjson/portfile.cmake
@@ -1,17 +1,19 @@
+set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_POLICY_SKIP_COPYRIGHT_CHECK enabled)
-set(VCPKG_POLICY_ALLOW_EMPTY_FOLDERS enabled)
-set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL https://github.com/Dir-A/CMakeModules.git
-    REF 8741cc048049608d1d92d9a9febd3372c2ac5ebd
+    URL https://github.com/ZQF-Zut/ZxJson.git
+    REF 372eb3c80affbaa9d75f42c32a0ae8296bcaa6e0
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME zqf_cmake_modules CONFIG_PATH share/cmake/zqf_cmake_modules)
+vcpkg_cmake_config_fixup(PACKAGE_NAME zqf_zut_zxjson CONFIG_PATH share/cmake/zqf_zut_zxjson)
+vcpkg_copy_pdbs()
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
