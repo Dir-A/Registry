79d00e00fb6e3d2a89166b7a3dca0c0f85a4b370
Dir-A <g2576612205@gmail.com>
2025-11-02 16:43:42 +0800

    update

--- ports/zqf-cmake-modules/portfile.cmake
+++ ports/zqf-cmake-modules/portfile.cmake
@@ -2,14 +2,7 @@ set(VCPKG_POLICY_SKIP_COPYRIGHT_CHECK enabled)
 set(VCPKG_POLICY_ALLOW_EMPTY_FOLDERS enabled)
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
-vcpkg_from_git(
-  OUT_SOURCE_PATH SOURCE_PATH
-  URL https://github.com/Dir-A/CMakeModules.git
-  REF 4067aa73469e41c2dacaad50d43fe27ace4fe30a
-  HEAD_REF master
-)
-
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/src/")
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME zqf_cmake_modules CONFIG_PATH share/cmake/zqf_cmake_modules)
 
