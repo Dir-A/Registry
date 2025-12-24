f066cf7283d995b76d520c2ae1683101950b57a1
Dir-A <g2576612205@gmail.com>
2025-12-24 21:33:22 +0800

    update

--- ports/zqf-cmake-modules/src/cmake/zqf_settings.cmake
+++ ports/zqf-cmake-modules/src/cmake/zqf_settings.cmake
@@ -1,6 +1,6 @@
 include_guard(GLOBAL)
 
-function(zqf_settings_output_dir)
+function(zqf_set_output_dir)
   if(CMAKE_SIZEOF_VOID_P EQUAL 4)
     set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}-32/" PARENT_SCOPE)
     set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}-32/" PARENT_SCOPE)
@@ -9,5 +9,36 @@ function(zqf_settings_output_dir)
     set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}-64/" PARENT_SCOPE)
     set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}-64/" PARENT_SCOPE)
     set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}-64/lib/" PARENT_SCOPE)
+  endif()
+endfunction()
+
+function(zqf_set_warning TARGET_NAME)
+  set(options)
+  set(multiValueArgs)
+  set(oneValueArgs
+    LEVEL
+  )
+  cmake_parse_arguments(ZQF "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  if(ZQF_LEVEL STREQUAL "AllExtra")
+    target_compile_options(${TARGET_NAME} PRIVATE
+      $<$<OR:$<C_COMPILER_ID:MSVC>,$<CXX_COMPILER_ID:MSVC>>:/W4> # msvc
+      $<$<OR:$<C_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:GNU>>:-Wall -Wextra> # gcc
+      $<$<OR:$<C_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:Clang>>: # clang
+      $<$<OR:$<C_COMPILER_FRONTEND_VARIANT:GNU>,$<CXX_COMPILER_FRONTEND_VARIANT:GNU>>:-Wall -Wextra> # gnu frontend
+      $<$<OR:$<C_COMPILER_FRONTEND_VARIANT:MSVC>,$<CXX_COMPILER_FRONTEND_VARIANT:MSVC>>:/W4> # msvc frontend
+      >
+    )
+  elseif(ZQF_LEVEL STREQUAL "All")
+    target_compile_options(${TARGET_NAME} PRIVATE
+      $<$<OR:$<C_COMPILER_ID:MSVC>,$<CXX_COMPILER_ID:MSVC>>:/W3> # msvc
+      $<$<OR:$<C_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:GNU>>:-Wall> # gcc
+      $<$<OR:$<C_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:Clang>>: # clang
+      $<$<OR:$<C_COMPILER_FRONTEND_VARIANT:GNU>,$<CXX_COMPILER_FRONTEND_VARIANT:GNU>>:-Wall> # gnu frontend
+      $<$<OR:$<C_COMPILER_FRONTEND_VARIANT:MSVC>,$<CXX_COMPILER_FRONTEND_VARIANT:MSVC>>:/W3> # msvc frontend
+      >
+    )
+  else()
+    message(FATAL_ERROR "zqf_set_warning: unknown warning level")
   endif()
 endfunction()
